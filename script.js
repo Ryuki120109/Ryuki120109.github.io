9981c4f287d58aa5bd36610fd4402189b6a0c2a7
Ryuki120109 <152758258+Ryuki120109@users.noreply.github.com>
2023-12-04 14:37:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const wrapper document.querySelector(".wrapper"); const question document.querySelector(".question"); const gif document.querySelector(".gif"); const yesBtn = document.querySelector(".yes-btn"); const noBtn = document.querySelector(".no-btn"); yesBtn.addEventListener("click", () { question.innerHTML = "Aaaaa, I like you too"; gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; }); noBtn.addEventListener("mouseover", () => { const noBtnRect noßtn.getBoundingClientRect(); const maxX = window.innerWidth const maxY = window.innerHeight noBtnRect.width; noßtnRect.height; const randomX = Math.floor(Math.random() maxx); const randomy Math.floor(Math.random() maxY); noBtn.style.left randomx + "px"; noBtn.style.top = randomY + "px"; });
+const wrapper document.querySelector(".wrapper");
+const question document.querySelector(".question");
+const gif document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+  question.innerHTML = "Aaaaa, I like you too";
+  gif.src =           "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
+   const noBtnRect = noßtn.getBoundingClientRect();
+   const maxX = window.innerWidth - noBtnRect.width;
+   const maxY = window.innerHeight - noßtnRect.height;
+   
+   const randomX = Math.floor(Math.random() * maxx);
+   const randomY = Math.floor(Math.random() * maxY);
+
+   noBtn.style.left = randomx + "px";
+   noBtn.style.top = randomY + "px";
+});
