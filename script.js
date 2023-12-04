dc8dd6006974d01a7f0136491b6de62bfbdbeceb
Ryuki120109 <152758258+Ryuki120109@users.noreply.github.com>
2023-12-04 14:50:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Aaaaa, I like you too";
   gif.src =   
- 
 "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";  
 });
 
